08415cc9ce0a206bed1eecc290f47be4a7e1aec7
byhj <476652422@qq.com>
2015-06-07 15:27:26 +0800

    Framework

--- src/ch1-1-Window/ch1-1-Window.cpp
+++ src/ch1-1-Window/ch1-1-Window.cpp
@@ -16,8 +16,7 @@ bool render(float timeDelta)
 {
 	if (Device)
 	{
-		Device->Clear(0, 0, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER,
-			          0x00000000, 1.0f, 0);
+		Device->Clear(0, 0, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, 0x00000000, 1.0f, 0);
 		Device->Present(0, 0, 0, 0); //backbuffer
 	}
 	return 0;
