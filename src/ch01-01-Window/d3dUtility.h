44e42098aefb2cbb4c3a39fe5d45d87e12f89c87
byhj <476652422@qq.com>
2015-12-26 10:46:58 +0800

    modifed framework

---
+++ src/ch01-01-Window/d3dUtility.h
@@ -0,0 +1,44 @@
+#ifndef D3DUTILITY_H
+#define D3DUTILITY_H
+
+#include <d3d9.h>
+
+namespace byhj
+{
+	//init the window
+	bool init(
+		HINSTANCE hInstance,        //application instance
+	    int width, int height,      //backe buffer dimensions
+		bool windowed,              //full window
+		D3DDEVTYPE deviceType,      //HAL or REF
+		IDirect3DDevice9 **device) ; //[out] the created device
+
+	// message loop
+	int EnterMsgLoop( bool (*ptr_render)(float timeDelta) );
+
+	//window procedure 
+	LRESULT CALLBACK WndProc(
+		HWND hwnd,
+	    UINT msg,
+		WPARAM wParam,
+		LPARAM lParam
+		);
+
+	template<class T>  void Release(T t)
+	{
+		if (t)
+		{
+			t->Release();
+			t = 0;
+		}
+	}
+	template<class T>  void Delete(T t)
+	{
+		if (t)
+		{
+			delete t;
+			t = 0;
+		}
+	}
+}
+#endif
